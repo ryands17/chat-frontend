beca20858ec01fcef23341628f9452c964b7a21f
Ryan <dsouza.ryan.11@gmail.com>
2021-01-16 14:44:00 +0530

    initial setup

---
+++ src/aws-exports-example.ts
@@ -0,0 +1,10 @@
+export const config = {
+  Auth: {
+    region: '',
+    userPoolId: '',
+    userPoolWebClientId: '',
+  },
+  aws_appsync_graphqlEndpoint: '',
+  aws_appsync_region: '',
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+}
